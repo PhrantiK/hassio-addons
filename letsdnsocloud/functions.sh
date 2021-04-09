b56ab1abd7ebb78e2fdfe4383d8b8029f42ffbc6
PhrantiK <PhrantiK@users.noreply.github.com>
2021-04-09 20:02:25 +0800

    interate over 0 fix

--- letsdnsocloud/functions.sh
+++ letsdnsocloud/functions.sh
@@ -10,7 +10,7 @@ function grabzoneid() {
   export ZONEID=$(curl -sX GET "https://api.cloudflare.com/client/v4/zones" \
     -H "X-Auth-Email: $CF_EMAIL" \
     -H "X-Auth-Key: $CF_APIKEY" \
-    -H "Content-Type: application/json" | jq -r '.result[] | (select(.name | contains("'$BASEDOMAIN'"))) | .id')
+    -H "Content-Type: application/json" | jq -r '.result[]? | (select(.name | contains("'$BASEDOMAIN'"))) | .id')
 }
 
 #Grab id from existing A record
@@ -19,7 +19,7 @@ function grabaid() {
   AID=$(curl -sX GET "https://api.cloudflare.com/client/v4/zones/$ZONEID/dns_records" \
     -H "X-Auth-Email: $CF_EMAIL"\
     -H "X-Auth-Key: $CF_APIKEY"\
-    -H "Content-Type: application/json" | jq -r '.result[] | (select(.name | contains("'$LE_DOMAINS'"))) | (select (.type | contains("A"))) | .id')
+    -H "Content-Type: application/json" | jq -r '.result[]? | (select(.name | contains("'$LE_DOMAINS'"))) | (select (.type | contains("A"))) | .id')
 
 }
 
