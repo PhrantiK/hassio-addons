9b3fa4a59bffd966378c05d90ea07062d35a7a52
PhrantiK <PhrantiK@users.noreply.github.com>
2021-04-09 23:52:36 +0800

    return domains on second page

--- letsdnsocloud/functions.sh
+++ letsdnsocloud/functions.sh
@@ -7,10 +7,10 @@ function grabzoneid() {
   BASEDOMAIN=$(sed 's/.*\.\(.*\..*\)/\1/' <<< $LE_DOMAINS)
 
   #Grab Zoneid & Export for Hooks.sh
-  export ZONEID=$(curl -sX GET "https://api.cloudflare.com/client/v4/zones" \
+  export ZONEID=$(curl -sX GET "https://api.cloudflare.com/client/v4/zones?per_page=50" \
     -H "X-Auth-Email: $CF_EMAIL" \
     -H "X-Auth-Key: $CF_APIKEY" \
-    -H "Content-Type: application/json" | jq -r '.result[]? | (select(.name | contains("'$BASEDOMAIN'"))) | .id')
+    -H "Content-Type: application/json" | jq -r '.result[] | (select(.name | contains("'$BASEDOMAIN'"))) | .id')
 }
 
 #Grab id from existing A record
@@ -19,7 +19,7 @@ function grabaid() {
   AID=$(curl -sX GET "https://api.cloudflare.com/client/v4/zones/$ZONEID/dns_records" \
     -H "X-Auth-Email: $CF_EMAIL"\
     -H "X-Auth-Key: $CF_APIKEY"\
-    -H "Content-Type: application/json" | jq -r '.result[]? | (select(.name | contains("'$LE_DOMAINS'"))) | (select (.type | contains("A"))) | .id')
+    -H "Content-Type: application/json" | jq -r '.result[] | (select(.name | contains("'$LE_DOMAINS'"))) | (select (.type | contains("A"))) | .id')
 
 }
 
